034d314684449d845566b210a9b73b51f99d49b3
tim <1510899867@qq.com>
2018-04-27 17:54:00 +0800

    EFLogger

---
+++ UnifyPermission/Code/Log/EFLogger.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnifyPermission.Options;
+
+namespace UnifyPermission.Code.Log
+{
+    public class EFLogger : ILogger
+    {
+        private EFLoggerOptions options;
+        public EFLogger(EFLoggerOptions options)
+        {
+            this.options = options;
+        }
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            if(logLevel > options.logLevel)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            var message = formatter(state, exception);
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnifyPermission/Code/Log/EFLoggerProvider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnifyPermission.Options;
+
+namespace UnifyPermission.Code.Log
+{
+    public class EFLoggerProvider : ILoggerProvider
+    {
+        private EFLoggerOptions options;
+        public EFLoggerProvider(EFLoggerOptions options)
+        {
+            this.options = options;
+        }
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new EFLogger(options);
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
